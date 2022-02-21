f7666653728106a8541a19a884dd21e7dcc9fab5
Raj Bhinde <68704186+irajbhinde@users.noreply.github.com>
2022-02-21 15:08:36 +0530

    refactor : changing the style from css instead of js, adding color class

--- Components/Snackbar/snackbar.js
+++ Components/Snackbar/snackbar.js
@@ -4,14 +4,14 @@ const leadingDemo = document.querySelector(".leadingdemo");
 const successSnackbar = document.querySelector(".success")
 
 baselineDemo.addEventListener("click", function baseLineHandler(){
-    errorSnackbar.style.cssText += 'display: flex; left: 34rem;bottom: 0.5rem;position: fixed;';
+    errorSnackbar.style.display = "flex";
     setTimeout(()=> {
         errorSnackbar.style.display = "none";
     },3000)
 });
 
 leadingDemo.addEventListener("click", function leadingHandler(){
-    successSnackbar.style.cssText += 'display: flex; left: 1rem;bottom: 0.5rem;position: fixed;';
+    successSnackbar.style.display = "flex";
     setTimeout(()=> {
         successSnackbar.style.display = "none";
     },3000)
